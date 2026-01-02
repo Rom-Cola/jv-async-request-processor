abc51d94d28dce900cc542185401e934f5f2be9c
Loiev Roman <loievroman@gmail.com>
2026-01-02 03:17:35 +0200

    Task completed.

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,6 +1,8 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
@@ -10,7 +12,22 @@ public class AsyncRequestProcessor {
         this.executor = executor;
     }
 
+    Map<String, UserData> cache = new ConcurrentHashMap<>();
+
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (cache.containsKey(userId)) {
+                return cache.get(userId);
+            } else {
+                UserData userData = new UserData(userId, "Some user");
+                cache.put(userId, userData);
+                return userData;
+            }
+        }, executor);
     }
 }
